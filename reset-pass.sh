95aba2a1cebfa516b112e5697ac5c019aaab611e
Mohankumar <mohanram@bin2bin.in>
2022-12-19 12:11:37 +0530

    backup

--- reset-pass.sh
+++ reset-pass.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 transmission-daemon \
     --port 8080 --auth --allowed "*" --no-incomplete-dir \
-    --username "admin" --password "$(cat /proc/sys/kernel/random/uuid)" \
+    --username "$1" --password "$2" \
     --foreground --watch-dir "/app/torrents-watch" \
     --download-dir "/app/data" --config-dir "/app/config" \
     --dump-settings &> "/app/config/settings.json.reset"
